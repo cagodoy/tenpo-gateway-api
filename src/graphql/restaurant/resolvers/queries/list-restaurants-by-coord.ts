9fae674cb8258d2081b314092e5c3ede1efa9d94
Camilo <cagodoy@acid.cl>
2019-12-19 03:30:43 -0300

    refactor, validate token before request

--- src/graphql/restaurant/resolvers/queries/list-restaurants-by-coord.ts
+++ src/graphql/restaurant/resolvers/queries/list-restaurants-by-coord.ts
@@ -1,21 +1,60 @@
 import RestaurantsSvc, { TRestaurantResponse, TCoord } from '../../../../clients/restaurants';
+import AuthSvc, { TAuthVerifyTokenResponse } from '../../../../clients/auth';
 
-const listRestaurantsByCoord = async (_, { input }) => {
+const listRestaurantsByCoord = async (root, { input }) => {
+  // validate if token is present
+  const { token } = root;
+  if (token === null) {
+    const e = 'token is null';
+    const res: TRestaurantResponse = {
+      data: null,
+      meta: null,
+      error: {
+        message: e,
+        code: 401,
+      },
+    };
+
+    console.log('[Query][listRestaurantsByCoord][Error] ', e);
+    return res;
+  }
+
+  // verify if token is valid
+  let auth: TAuthVerifyTokenResponse;
   try {
-    const { latitude, longitude, pageToken } = input;
+    auth = await AuthSvc.verifyToken(token);
+    if (auth.valid === false) {
+      throw new Error(auth.error.message);
+    }
+  } catch (err) {
+    const res: TRestaurantResponse = {
+      data: null,
+      meta: null,
+      error: {
+        message: err.message,
+        code: 401,
+      },
+    };
+
+    console.log('[Query][listRestaurantsByCoord][Error] ', err.message);
+    return res;
+  }
+
+  try {
+    const { latitude, longitude } = input;
 
     const coord: TCoord = {
       latitude,
       longitude,
     };
 
-    const payload = await RestaurantsSvc.listByCoord(coord, pageToken);
-    console.log('[Query][listRestaurantsByCoord][Payload] ', payload);
+    const payload = await RestaurantsSvc.listByCoord(coord, auth.data.userId);
+    console.log('[Query][listRestaurantsByCoord][Payload]', payload.data.length, 'restaurants');
 
     return payload;
   } catch (err) {
     if (err.message === 'client[name] is not a function') {
-      const e = `fail auth-api connection with host=${process.env.AUTH_HOST} and port=${process.env.AUTH_PORT}`;
+      const e = `fail restaurants-api connection with host=${process.env.RESTAURANTS_HOST} and port=${process.env.RESTAURANTS_PORT}`;
 
       const res: TRestaurantResponse = {
         data: null,
